3e3dfc8c23491bfc09c8dfeb283b85584354ec0d
00016197 <00016197wiut@gmail.com>
2024-03-11 23:22:10 +0500

    Started serving HTML

---
+++ app.js
@@ -0,0 +1,29 @@
+
+
+const express = require('express')
+
+// parse request body to json
+const body_parser = require('body-parser')
+
+// for File IO
+const path = require('path')
+
+// for web routing
+const web_route = require('./routes/web')
+
+// make mock database (raw .json file) available globally in app
+global.mock_db = path.join(__dirname, './data/mock_db.json');
+
+const app = express();
+
+// Set the view engine for web routes
+app.set('view engine', 'pug');
+
+app.use('/css', express.static('public/CSS'))
+app.use('/js', express.static('public/JS'))
+
+app.use('/', web_route); // web routes
+
+const port = 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
