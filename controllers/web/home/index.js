da9ed4f2170e82be36e7b6ece2fc16eab509df61
00016197 <00016197wiut@gmail.com>
2024-03-11 23:55:22 +0500

    Add/Update View ha been added

--- controllers/web/home/index.js
+++ controllers/web/home/index.js
@@ -1,6 +1,15 @@
+
+
+
 const home_controller = {
     index: async (req, res) => {
         res.render('home');
+    },
+    add: async (req, res) => {
+        res.render('home/add_update');
+    },
+    update: async (req, res) => {
+        res.render('home/add_update');
     }
 };
 
